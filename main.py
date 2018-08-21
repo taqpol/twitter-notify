fa4adc417e8a21ae3d725d1f015a9c60c3565b1a
taqpol <njosephchen@gmail.com>
2018-08-21 01:09:53 -0700

    initial commit
    test commits

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask 
+from twilio.rest import Client
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def main():
+	print(request.data)
+	# parse_post_data(request.data)
+
+def send_text():
+	account_sid = os.environ.get('account_sid')
+	auth_token  = os.environ.get('auth_token')
+
+	client = Client(account_sid, auth_token)
+
+	message = client.messages.create(
+	    to=os.environ.get('receiving_number'), 
+	    from_=os.environ.get('sending_number'),
+	    body="Hello from Python!")
+
+	print(message.sid)
+
+def parse_post_data(post_data):
+	print(post_data)
+
+if __name__ == '__main__':
+	app.run()
